e87ae559592feb952ad12c0419805d4b6b7e93a8
Meir <maornet@gmail.com>
2025-05-30 17:35:18 +0300

    update

--- crates/rustflow_parser/src/netflow_v9.rs
+++ crates/rustflow_parser/src/netflow_v9.rs
@@ -174,7 +174,6 @@ fn parse_data_flow_set(
         let template_record = templates.get(&flow_set_id).unwrap();
 
         let (_, records) = many0(parse_data_record(template_record)).parse(input)?;
-        // let (_, padding) = all_consuming(take_while(|i: u8| i == 0)).parse(input)?;
 
         Ok((
             rest,
@@ -248,7 +247,6 @@ fn parse_options_template_flow_set(input: &[u8]) -> IResult<&[u8], FlowSet> {
         verify(be_u16, |i| *i == OPTIONS_TEMPLATE_FLOW_SET_ID).parse(input)?;
     let (input, length) = be_u16(input)?;
     let (_, records) = many0(parse_options_template_record).parse(input)?;
-    // let (_, padding) = all_consuming(take_while(|i: u8| i == 0)).parse(input)?;
 
     Ok((
         rest,
